d2e7419a700e1fde614c94b00b5727dd6c0e17a0
Konstantin Gagarin <gagarinska@gmail.com>
2015-12-14 22:04:01 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get install mc zsh git mercurial python-dev python-setuptools python-virtualenv python3-virtualenv python3-dev python3-setuptools
+
+#oh-my-zsh <3 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Packages for psycopg2 in virtualenv
+sudo apt-get install libpq-dev python-dev
+
+#Packages for Pillow in virtualenv
+sudo apt-get install libjpeg8 libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
+
+cd ~/src/
+
+ln -s ~/src/dotfiles/vimrc ~/.vimrc
+ln -s ~/src/dotfiles/vim ~/.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
