9d2430f9eac006e9e6cc4e686f2b4d674aa55cb0
xggrnx <gagarinska@gmail.com>
2018-09-14 09:31:57 +0300

    changes in install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 sudo apt-get install mc zsh git mercurial vim exuberant-ctags curl wget libcurl4-openssl-dev
 
 #Python Dev Modules
-sudo apt-get install python-dev python-setuptools python-virtualenv python3-virtualenv python3-dev python3-setuptools 
+sudo apt-get install python-dev python-setuptools python-virtualenv python3-virtualenv python3-dev python3-setuptools
 
 #Node
-sudo apt-get install npm nodejs nodejs-legacy 
+sudo apt-get install npm nodejs nodejs-legacy
 
 
 #Packages for psycopg2 in virtualenv
-sudo apt-get install libpq-dev python-dev
+sudo apt-get install libpq-dev
 
 #Packages for lxml
 sudo apt-get install libxml2-dev libxslt-dev
@@ -19,7 +19,6 @@ sudo apt-get install libxml2-dev libxslt-dev
 #Packages for Pillow in virtualenv
 sudo apt-get install libjpeg8 libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
 
-cd ~/src/
 
 ln -s ~/src/dotfiles/vimrc ~/.vimrc
 ln -s ~/src/dotfiles/vim ~/.vim
@@ -29,7 +28,6 @@ vim +PluginInstall +qall
 
 git clone https://github.com/powerline/fonts.git
 cd ~/src/fonts
-sh install.sh
 
 echo "installing fonts at $PWD to ~/.fonts/"
 mkdir -p ~/.fonts/adobe-fonts/source-code-pro
@@ -38,10 +36,17 @@ git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-
 fc-cache -f -v ~/.fonts/adobe-fonts/source-code-pro
 echo "finished installing"
 
-#pyEnv
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 #oh-my-zsh <3 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #Exit from zsh after install
+
+touch ~/.zshalias
+
+cp ~/src/dotfiles/pocahontas.zsh-theme ~/.oh-my-zsh/themes/
+
+mkdir ~/.pyenv
+
+pip3 install virtualenvwrapper
+
 exit
